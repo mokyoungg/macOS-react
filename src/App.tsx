9599068b7505b81f65578813475db50161ddf2d3
mokyoungg <mokyoungg@gmail.com>
2025-02-11 18:03:14 +0900

    First Commit!

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import classNames from "classnames/bind";
+import styles from "./App.module.scss";
+
+const cx = classNames.bind(styles);
+
+function App() {
+  return <div className={cx("container")}>App</div>;
+}
+
+export default App;
